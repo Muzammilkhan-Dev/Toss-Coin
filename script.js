311cbed5490652f9f35456d4ac93045035dee07c
muzammil99754011-collab <muzammil.99754011@gmail.com>
2026-02-08 15:17:10 +0530

    bounce added

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ let toss = document.querySelector("#toss");
 let cho = document.querySelector("#cho");
 
 let ans = () => {
+
+    cho.classList.remove("animate");
+    void cho.offsetWidth;
+    cho.classList.add("animate");
+    cho.innerText = "";
+
     let ans = Math.floor(Math.random() * 2);
-    console.log(ans);
-    if(ans === 0){
-        cho.innerText = "HEAD"
-    } else {
-        cho.innerText = "TAIL"
-    }
+   
+    setTimeout(() => {
+        cho.innerText = ans === 0 ? "Head" : "Tail";
+    },900)
+
+    
+
 }
 
 toss.addEventListener("click", () => {
